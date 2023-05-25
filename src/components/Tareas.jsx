5c0f002402d373c1a672e5ca22e6e54c5b727cf5
Joel Jarro <rodrigoquiroz308@gmail.com>
2023-05-25 13:31:32 -0400

    add underline to tareas and props to tarea

--- src/components/Tareas.jsx
+++ src/components/Tareas.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { IconButton } from "./IconButton";
 import plus from "../assets/add.svg";
+import { Tarea } from "./Tarea";
 
 export const Tareas = () => {
   const [tareas, setTareas] = useState([]);
@@ -9,7 +10,13 @@ export const Tareas = () => {
       <div className="flex flex-col items-center h-screen p-10">
         <span className="text-3xl font-bold">Tareas</span>
         {tareas.map((tarea) => {
-          return <Tarea tarea={tarea} />;
+          return (
+            <Tarea
+              nombre={tarea.nombre}
+              setTareas={setTareas}
+              tareas={tareas}
+            />
+          );
         })}
       </div>
       <div className="absolute right-3 bottom-3 w-10 h-10">
