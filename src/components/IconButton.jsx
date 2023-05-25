13edd16722092babef8afda8eb6be610e787850f
Joel Jarro <rodrigoquiroz308@gmail.com>
2023-05-25 13:14:32 -0400

    fix Icon Button

--- src/components/IconButton.jsx
+++ src/components/IconButton.jsx
@@ -1,4 +1,10 @@
-
-export const IconButton = ({onClick, src}) => {
-  return <img src={src} alt="cerrar modal" onClick={onClick} />;
+export const IconButton = ({ onClick, src }) => {
+  return (
+    <img
+      className="rounded-sm p-1 fill-zinc-600"
+      src={src}
+      alt="cerrar modal"
+      onClick={onClick}
+    />
+  );
 };
