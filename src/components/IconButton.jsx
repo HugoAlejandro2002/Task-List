980d7ecccc51f7d4e126c375e627c13f28b6c559
lolex <apazahuaychohugoalejandro@gmail.com>
2023-05-25 13:16:52 -0400

    obligado por git

--- src/components/IconButton.jsx
+++ src/components/IconButton.jsx
@@ -1,4 +1,4 @@
 
 export const IconButton = ({onClick, src}) => {
-  return <img src={src} alt="cerrar modal" onClick={onClick} />;
+  return <img className='text-red-700' src={src} alt="cerrar modal" onClick={onClick} />;
 };
