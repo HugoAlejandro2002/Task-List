980be4cc6b3e124cf1b3a85b8cf4e8152654f557
lolex <apazahuaychohugoalejandro@gmail.com>
2023-05-25 13:02:08 -0400

    add tarea component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import "./App.css";
 import { IconButton } from "./components/IconButton";
 import AddImg from "./assets/add.svg"
+import { Tarea } from "./components/tarea";
 
 function App() {
   return (
     <>
-      <div className="absolute right-3 bottom-3 w-10 h-10">
-        <IconButton src={AddImg} onClick={()=>{console.log("Add Tarea")}}/>
-      </div>
+      <Tarea/>
     </>
   );
 }
---
+++ src/components/Tarea.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { IconButton } from './IconButton'
+import check from '../assets/check.svg'
+import cross from '../assets/cross.svg'
+
+
+export const Tarea = () => {
+    return (
+        <div className='flex flex-row justify-around align-middle w-22 my-auto rounded-md border-2 border-solid border-gray-400'>
+            <div>
+                Tarea 1
+            </div>
+            <div className='flex flex-row w-20 h-5 justify-around'>
+                <IconButton src={check} onClick={() => console.log('check')} />
+                <IconButton src={cross} onClick={() => console.log('cross')}/>
+
+            </div>
+        </div>
+    )
+}
